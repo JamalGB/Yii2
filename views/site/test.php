93cc5c03dc22ed296b94bbfff411623664ecfe54
Jamal Absalimov <41488590+JamalGB@users.noreply.github.com>
2018-07-21 18:49:49 +0300

    Загрузка  проекта в GitHub

---
+++ views/site/test.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    $this->title = "Тестовая страница";
+?>
+
+<div>
+    
+    <h2>Курс по yii2 на портале GeekBrains</h2>
+    <a href="https://geekbrains.ru/courses/35" target="_blank"> Перейти</a>
+</div>
